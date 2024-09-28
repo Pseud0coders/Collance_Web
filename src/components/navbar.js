40ab4ebc7e658e9ece58f61ffa7526969d6bc420
priyanshubarai <priyanshubarai45@gmail.com>
2024-09-29 02:57:32 +0530

    Hero section done

--- src/components/navbar.js
+++
@@ -1,7 +0,0 @@
-import img1 from '../Collance_assets/Hero Section/NAV BAR/LOGO.png'
-
-export default function Navbar(){
-    return(
-        <img src={img1} alt="" />
-    )
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-blue': 'rgba(3, 4, 94, 1)',
+      },
+    },
   },
   plugins: [],
 }
