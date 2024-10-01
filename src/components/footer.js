62bd8b5ca6fc5170246e8d20701e83e615e387c4
priyanshubarai <priyanshubarai45@gmail.com>
2024-10-02 04:42:58 +0530

    footer bn gya bkl

---
+++ src/components/footer.js
@@ -0,0 +1,47 @@
+import './footer.css'
+import img from '../Collance_assets/Footer/LOGO.png'
+import social from '../Collance_assets/Footer/socials.png'
+
+export default function Footer() {
+    return(
+        <div className="footer w-screen grid">
+            <img className='social_ref absolute' src={social} alt="" />
+            <div className="footer_info absolute">&copy; 2024 Collance.com . All rights reserverd.</div>
+            <img className='footer_logo absolute w-64 h-16' src={img} alt="" />
+            <div className="collance grid justify-center">
+                <h3 className="text-lg">Collance</h3>
+                <a className="text-sm" href="">Pricing</a>
+                <a className="text-sm" href="">Updates</a>
+                <a className="text-sm" href="">Beta</a>
+                <a className="text-sm" href="">Newsletter</a>
+                <a className="text-sm" href="">Collaborations</a>
+            </div>
+            <div className="product grid justify-center">
+                <h3 className="text-lg">Product</h3>
+                <a className="text-sm" href="">Clients</a>
+                <a className="text-sm" href="">Freelancers</a>
+                <a className="text-sm" href="">Newcomers</a>
+            </div>
+            <div className="learning grid justify-center">
+                <h3 className="text-lg">Learning</h3>
+                <a className="text-sm" href="">Learn</a>
+                <a className="text-sm" href="">Tutorials</a>
+                <a className="text-sm" href="">Communities</a>
+            </div>
+            <div className="resources grid justify-center">
+                <h3 className="text-lg">Resources</h3>
+                <a className="text-sm" href="">Tutorials</a>
+                <a className="text-sm" href="">Editorials</a>
+                <a className="text-sm" href="">Product</a>
+                <a className="text-sm" href="">Newsroom</a>
+            </div>
+            <div className="about grid justify-center">
+                <h3 className="text-lg">About</h3>
+                <a className="text-sm" href="">Company</a>
+                <a className="text-sm" href="">Careers</a>
+                <a className="text-sm" href="">Legal</a>
+                <a className="text-sm" href="">Help</a>
+            </div>
+        </div>
+    )
+}
