1843d79dc7c74726e45f7ba62bd60365a083dbf0
unknown <mohammed885hamza@gmail.com>
2024-10-01 12:49:12 +0530

    new component added

---
+++ src/components/findthemgigs.js
@@ -0,0 +1,18 @@
+import './findthemgigs.css'
+
+export default function FindThemGigs(){
+    return(
+     <div class="pick_them_gigs">
+        <div class="content">
+            <div class="header">PICK THEM GIGS</div>
+            <div class="text">text</div>
+            <div class="footer_btn">
+                <div class="btn">button</div>
+                <div class="btn">button</div>
+                <div class="btn">button</div>
+            </div>
+        </div>
+        <div class="picture"></div>
+     </div>
+    )
+}
